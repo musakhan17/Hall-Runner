10b5a70788b7762508beccb0aee69a57074e4f95
Unknown <nhcung@knox.edu>
2018-05-01 15:00:07 -0500

    Re-import Item; Fix reward scene

---
+++ Assets/Scripts/ObjectShowup.cs
@@ -0,0 +1,42 @@
+﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectShowup : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _necklace;
+    [SerializeField]
+    private GameObject _descript;
+    [SerializeField]
+    private GameObject _nextbutton;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private
+
+    // Use this for initialization 
+    void Start()
+    {
+        _descript.SetActive(false);
+        _nextbutton.SetActive(false);
+        StartCoroutine(Showup());
+    }
+
+    // Update is called once per frame 
+    IEnumerator Showup()
+    {
+        yield return new WaitForSeconds(5);
+        Debug.Log("Wait is over");
+        _descript.SetActive(true);
+        _nextbutton.SetActive(true);
+    }
+    void Update()
+    {
+        Vector3 newPos = new Vector3(-213.0f, -117.0f, 0f);
+        //float step = speed * Time.deltaTime; 
+        _necklace.transform.position = _necklace.transform.position + newPos;
+    }
+}
