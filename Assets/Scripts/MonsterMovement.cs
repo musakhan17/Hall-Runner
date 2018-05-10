685fdc2022141cbc8c8fde953d9cdc2763b67110
Lydia <tyha@knox.edu>
2018-05-10 15:46:00 -0500

    Remove unnecessary assets
    Also added JumpScare and MonsterMovement scripts

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _monster;
+    public static float moveSpeed = 0.8f;
+    public Vector3 aiDirection = Vector3.forward;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+        _monster.SetActive(true);
+        Invoke("Disappear", 20);
+
+    }
+
+    private void Disappear()
+    {
+        _monster.SetActive(false);
+    }
+
+}
